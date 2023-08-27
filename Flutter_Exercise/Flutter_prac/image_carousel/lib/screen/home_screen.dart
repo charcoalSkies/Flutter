a7a5d3c6f0714b29da7b6ccf0a8fa1cc3c311ab7
charcoalSkies <jun48470@gmail.com>
2023-08-27 23:46:38 +0900

    test : image_carousel 연습

--- Flutter_Exercise/Flutter_prac/image_carousel/lib/screen/home_screen.dart
+++ Flutter_Exercise/Flutter_prac/image_carousel/lib/screen/home_screen.dart
@@ -1,31 +1,28 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
   PageController controller = PageController(
     initialPage: 0,
   );
-  Timer? timer;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+    timer = Timer.periodic(Duration(seconds: 3), (timer) {
       int currentPage = controller.page!.toInt();
       int nextPage = currentPage + 1;
       if (nextPage > 4) {
         nextPage = 0;
       }
-
       controller.animateToPage(
         nextPage,
         duration: Duration(milliseconds: 400),
@@ -36,10 +33,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void dispose() {
+    controller.dispose();
     if (timer != null) {
       timer!.cancel();
     }
-    controller.dispose();
     // TODO: implement dispose
     super.dispose();
   }
