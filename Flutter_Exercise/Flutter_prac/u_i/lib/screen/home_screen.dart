9c2b03db1fb26216fa74a84f77ef9598b9c48b09
charcoalSkies <jun48470@gmail.com>
2023-08-30 21:49:23 +0900

    test : u_i 연습

--- Flutter_Exercise/Flutter_prac/u_i/lib/screen/home_screen.dart
+++ Flutter_Exercise/Flutter_prac/u_i/lib/screen/home_screen.dart
@@ -9,6 +9,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime seletedDate = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +25,10 @@ class _HomeScreenState extends State<HomeScreen> {
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              _TopPart(),
+              _TopPart(
+                seletedDate: seletedDate,
+                onPressed: onHeartPressed,
+              ),
               _BottomPart(),
             ],
           ),
@@ -27,10 +36,47 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  void onHeartPressed() {
+    DateTime now = DateTime.now();
+    showCupertinoDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 300.0,
+              color: Colors.white,
+              child: CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.date,
+                initialDateTime: seletedDate,
+                maximumDate: DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                ),
+                onDateTimeChanged: (DateTime date) {
+                  // print(date);
+                  seletedDate = date;
+                  print(seletedDate);
+                },
+              ),
+            ),
+          );
+        });
+  }
 }
 
 class _TopPart extends StatelessWidget {
-  const _TopPart({super.key});
+  final DateTime seletedDate;
+  final VoidCallback onPressed;
+
+  _TopPart({
+    required this.seletedDate,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +87,7 @@ class _TopPart extends StatelessWidget {
           Text(
             'U & I',
             style: TextStyle(
+              color: Colors.white,
               fontSize: 80.0,
               fontFamily: 'parisienne',
             ),
@@ -50,6 +97,7 @@ class _TopPart extends StatelessWidget {
               Text(
                 '우리 처음 만난날',
                 style: TextStyle(
+                  color: Colors.white,
                   fontSize: 30.0,
                   fontFamily: 'sunflower',
                 ),
@@ -57,6 +105,7 @@ class _TopPart extends StatelessWidget {
               Text(
                 '2023.08.24',
                 style: TextStyle(
+                  color: Colors.white,
                   fontSize: 20.0,
                   fontFamily: 'sunflower',
                 ),
@@ -64,27 +113,8 @@ class _TopPart extends StatelessWidget {
             ],
           ),
           IconButton(
-            onPressed: () {
-              // Dialog
-              showCupertinoDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        height: 300.0,
-                        color: Colors.white,
-                        child: CupertinoDatePicker(
-                          mode: CupertinoDatePickerMode.date,
-                          onDateTimeChanged: (DateTime time) {
-                            print(time);
-                          },
-                        ),
-                      ),
-                    );
-                  });
-            },
             iconSize: 60.0,
+            onPressed: onPressed,
             icon: Icon(
               Icons.favorite,
               color: Colors.red,
@@ -93,10 +123,10 @@ class _TopPart extends StatelessWidget {
           Text(
             'D + 1',
             style: TextStyle(
-              fontSize: 50.0,
-              fontFamily: 'sunflower',
-              fontWeight: FontWeight.w700,
-            ),
+                color: Colors.white,
+                fontSize: 50.0,
+                fontFamily: 'sunflower',
+                fontWeight: FontWeight.w700),
           ),
         ],
       ),
