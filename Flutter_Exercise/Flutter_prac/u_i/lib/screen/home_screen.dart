530cad93c0e9eef3d8fe6917898c87b388c9712b
charcoalSkies <jun48470@gmail.com>
2023-09-01 00:08:28 +0900

    test: u & i 연습

--- Flutter_Exercise/Flutter_prac/u_i/lib/screen/home_screen.dart
+++ Flutter_Exercise/Flutter_prac/u_i/lib/screen/home_screen.dart
@@ -9,12 +9,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime seletedDate = DateTime(
+  DateTime selectedDate = DateTime(
     DateTime.now().year,
     DateTime.now().month,
     DateTime.now().day,
   );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,60 +22,57 @@ class _HomeScreenState extends State<HomeScreen> {
         bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              _TopPart(
-                seletedDate: seletedDate,
-                onPressed: onHeartPressed,
-              ),
-              _BottomPart(),
-            ],
-          ),
+          child: Column(children: [
+            _TopPart(
+              seletedDate: selectedDate,
+              onPressed: onHeartClicked,
+            ),
+            _BottomPart(),
+          ]),
         ),
       ),
     );
   }
 
-  void onHeartPressed() {
-    DateTime now = DateTime.now();
+  void onHeartClicked() {
+    final DateTime now = DateTime.now();
     showCupertinoDialog(
-        context: context,
-        barrierDismissible: true,
-        builder: (BuildContext context) {
-          return Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 300.0,
-              color: Colors.white,
-              child: CupertinoDatePicker(
-                mode: CupertinoDatePickerMode.date,
-                initialDateTime: seletedDate,
-                maximumDate: DateTime(
-                  now.year,
-                  now.month,
-                  now.day,
-                ),
-                onDateTimeChanged: (DateTime date) {
-                  // print(date);
-                  seletedDate = date;
-                  print(seletedDate);
-                },
+      context: context,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: Colors.white,
+            height: 300.0,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: selectedDate,
+              maximumDate: DateTime(
+                now.year,
+                now.month,
+                now.day,
               ),
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              },
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
 
 class _TopPart extends StatelessWidget {
   final DateTime seletedDate;
   final VoidCallback onPressed;
-
   _TopPart({
-    required this.seletedDate,
-    required this.onPressed,
-    Key? key,
-  }) : super(key: key);
+    required DateTime this.seletedDate,
+    required VoidCallback this.onPressed,
+    Key? keys,
+  }) : super(key: keys);
 
   @override
   Widget build(BuildContext context) {
@@ -88,8 +84,8 @@ class _TopPart extends StatelessWidget {
             'U & I',
             style: TextStyle(
               color: Colors.white,
-              fontSize: 80.0,
               fontFamily: 'parisienne',
+              fontSize: 80.0,
             ),
           ),
           Column(
@@ -98,35 +94,35 @@ class _TopPart extends StatelessWidget {
                 '우리 처음 만난날',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 30.0,
                   fontFamily: 'sunflower',
+                  fontSize: 30.0,
                 ),
               ),
               Text(
-                '2023.08.24',
+                '2023.09.01',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 20.0,
                   fontFamily: 'sunflower',
+                  fontSize: 20.0,
                 ),
               ),
             ],
           ),
           IconButton(
-            iconSize: 60.0,
-            onPressed: onPressed,
-            icon: Icon(
-              Icons.favorite,
-              color: Colors.red,
-            ),
-          ),
+              onPressed: onPressed,
+              iconSize: 50.0,
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+              )),
           Text(
             'D + 1',
             style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontFamily: 'sunflower',
-                fontWeight: FontWeight.w700),
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ],
       ),
