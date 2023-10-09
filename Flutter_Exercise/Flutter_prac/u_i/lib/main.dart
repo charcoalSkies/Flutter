ee69b35efee15aea8cf5646cd1e24e918c8a9519
charcoalSkies <jun48470@gmail.com>
2023-10-09 23:58:08 +0900

    feat : flutter update, IOS 17 update

--- Flutter_Exercise/Flutter_prac/u_i/lib/main.dart
+++ Flutter_Exercise/Flutter_prac/u_i/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:u_i/screen/home_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    ),
-  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
 }
